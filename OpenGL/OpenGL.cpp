9b912553b35059877caccf1de1f8085bc76919f8
Makarray <mattshrider@gmail.com>
2013-02-03 18:53:10 -0500

    First real commit

--- OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL.cpp
@@ -1,17 +1,33 @@
-// OpenGL.cpp : Defines the entry point for the console application.
-//
+/**************************************************
+* OpenGL.cpp - First project of OpenGL
+*
+* Create objects that mirror a digital photograph
+* Authors: Matthew Shrider and James Uhe
+* ************************************************/
 
-#include "stdafx.h"
+#include "stdafx.h"	//for visual c++, just delete this if not on visual studio
 #include "gl/glut.h"
 
+//Functions
 void keyboard(unsigned char key, int x, int y);
 void display(void);
+void createObjects();
+void init();
 
 
-int main(int argc, char** argv)
-{
+GLint viewport[4];
+GLdouble mvMatrix[16];
+GLdouble prMatrix[16];
+
+
+int main(int argc, char** argv){
   glutInit(&argc, argv);
-  glutCreateWindow("GLUT Test");
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+  glutInitWindowSize(800,650);
+  glutCreateWindow("Castle by Matthew Shrider and James Uhe");
+
+  createObjects();
+
   glutKeyboardFunc(&keyboard);
   glutDisplayFunc(&display);
   glutMainLoop();
@@ -20,8 +36,7 @@ int main(int argc, char** argv)
 }
 
 
-void keyboard(unsigned char key, int x, int y)
-{
+void keyboard(unsigned char key, int x, int y){
   switch (key)
   {
     case '\x1B':
@@ -30,20 +45,54 @@ void keyboard(unsigned char key, int x, int y)
   }
 }
 
+void init(){
+   	
+}
 
-void display()
-{
-  glClear(GL_COLOR_BUFFER_BIT);
+void display(){
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glPolygonMode(GL_FRONT, GL_FILL);
 
-  glColor3f(1.0f, 0.0f, 0.0f);
-
-  glBegin(GL_POLYGON);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f( 0.5f, -0.5f);
-    glVertex2f( 0.5f,  0.5f);
-    glVertex2f(-0.5f,  0.5f);
+  //just a test
+  glBegin(GL_QUAD_STRIP);
+	glColor3f(0.0,0.8,0.8);
+	glVertex2f(-0.5,-.5);
+	glVertex2f(.5,-.5);
+	glVertex2f(-.5,.5);
+	glVertex2f(.5,.5);
   glEnd();
 
+
   glFlush();
+  glutSwapBuffers();
+}
+
+void resize (int w, int h){
+	glViewport(0,0,(GLint) w, (GLint) h);
+	glGetIntegerv(GL_VIEWPORT, viewport);
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity();
+	GLfloat ratio;
+	if (w <= h){
+		ratio = static_cast<GLfloat> (h) / w;
+	} else {
+		ratio = static_cast<GLfloat> (w) / h;
+		gluPerspective(60, ratio,-3,3);
+	}
+	glGetDoublev(GL_PROJECTION_MATRIX, prMatrix);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glGetDoublev(GL_MODELVIEW_MATRIX, mvMatrix);
+}
+
+
+
+void createObjects(){
+	GLuint list_fenceSpike;
+	list_fenceSpike = glGenLists(1);
+
+
+
+
 }
 
