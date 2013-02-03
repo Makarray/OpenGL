c73c003b223f5dd08f91a54b5526fb50bc059d2e
Makarray <mattshrider@gmail.com>
2013-02-03 13:54:45 -0500

    FirstFIle

---
+++ OpenGL/OpenGL.cpp
@@ -0,0 +1,49 @@
+// OpenGL.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "gl/glut.h"
+
+void keyboard(unsigned char key, int x, int y);
+void display(void);
+
+
+int main(int argc, char** argv)
+{
+  glutInit(&argc, argv);
+  glutCreateWindow("GLUT Test");
+  glutKeyboardFunc(&keyboard);
+  glutDisplayFunc(&display);
+  glutMainLoop();
+
+  return EXIT_SUCCESS;
+}
+
+
+void keyboard(unsigned char key, int x, int y)
+{
+  switch (key)
+  {
+    case '\x1B':
+      exit(EXIT_SUCCESS);
+      break;
+  }
+}
+
+
+void display()
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glColor3f(1.0f, 0.0f, 0.0f);
+
+  glBegin(GL_POLYGON);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f( 0.5f, -0.5f);
+    glVertex2f( 0.5f,  0.5f);
+    glVertex2f(-0.5f,  0.5f);
+  glEnd();
+
+  glFlush();
+}
+
